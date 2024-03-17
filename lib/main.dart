a0d1db9e145e29e0a41773da17d2e3e52fdb1b3f
tanjilalom <tanjil15-3079@diu.edu.bd>
2024-03-17 23:44:00 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _first = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedCrossFade(
+              duration: Duration(seconds: 2),
+              firstChild: Container(
+                  height: 300,
+                  child: Image.network(
+                      "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")),
+              secondChild: Container(
+                  height: 300,
+                  child: Image.network(
+                      "https://plus.unsplash.com/premium_photo-1676637000058-96549206fe71?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")),
+              crossFadeState:
+                  _first ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+            ),
+            SizedBox(height: 50,),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _first = !_first;
+                  });
+                },
+                child: Text(
+                  "Press",
+                  style: TextStyle(fontSize: 20),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
